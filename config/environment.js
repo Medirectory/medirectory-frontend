b40fbc75e869e06c9b2cd6d41676c0029e118480
thecristen <cristen@outlook.com>
2015-08-24 12:58:28 -0400

    use Lato font

--- config/environment.js
+++ config/environment.js
@@ -22,10 +22,10 @@ module.exports = function(environment) {
     contentSecurityPolicy: {
       'default-src': "'none'",
       'script-src': "'self'",
-      'font-src': "'self'",
+      'font-src': "'self' data: fonts.gstatic.com",
       'connect-src': "'self'",
       'img-src': "'self'",
-      'style-src': "'self' 'unsafe-inline'",
+      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com",
       'media-src': "'self'",
       'frame-src': "maps.google.com www.google.com/maps"
     }
